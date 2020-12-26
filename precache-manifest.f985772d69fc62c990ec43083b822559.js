29cdbd74cb1916356083f707fdc1636fb0b90f7d
architect <59576908+BeKateryna@users.noreply.github.com>
2020-12-26 17:52:27 +0200

    Add files via upload

---
+++ precache-manifest.f985772d69fc62c990ec43083b822559.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf63e7b7f19421582a998e35f4f3d51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6a0639104633ecad2b0",
+    "url": "/static/css/main.b8418060.chunk.css"
+  },
+  {
+    "revision": "3b5cecf366b22fecf1e4",
+    "url": "/static/js/2.3151f1a1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.3151f1a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6a0639104633ecad2b0",
+    "url": "/static/js/main.ad441dc0.chunk.js"
+  },
+  {
+    "revision": "c7474c3876da4b5e14ab",
+    "url": "/static/js/runtime-main.bceafa69.js"
+  },
+  {
+    "revision": "0798a3774e43050a5384f8ba77653c92",
+    "url": "/static/media/RC-NEWS-Growing-Smarter-report-released-0219.0798a377.jpg"
+  },
+  {
+    "revision": "afce843d7cadad787b6d3848ff23609c",
+    "url": "/static/media/star-1.afce843d.svg"
+  },
+  {
+    "revision": "ca05b0338660aed37e25c6e4c6bbdd4b",
+    "url": "/static/media/star-2.ca05b033.svg"
+  }
+]);
